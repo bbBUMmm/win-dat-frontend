9c02b888bf2b49d16b7d2a4ba6460f3285775fdc
Mykyta Prykhodko <dlagamekontrol@gmail.com>
2025-04-17 19:51:40 +0200

    User, Lobby models

---
+++ src/app/core/models/lobby-model.ts
@@ -0,0 +1,10 @@
+import {UserModel} from './user-model';
+
+export interface LobbyModel {
+  id: number;
+  name: string;
+  created: Date;
+  updated: Date;
+  closed: Date;
+  userList: UserModel[];
+}
