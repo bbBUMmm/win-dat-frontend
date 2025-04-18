da499f15c9665e0f466049773c35cd68a663c335
Mykyta Prykhodko <dlagamekontrol@gmail.com>
2025-04-18 23:42:02 +0200

    Connection of the SpartanUI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
 import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { lucideBox, lucideTriangleAlert } from '@ng-icons/lucide';
 
 import { routes } from './app.routes';
 import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {DefaultOAuthInterceptor, OAuthModule} from 'angular-oauth2-oidc';
+import {NgIconsModule, provideIcons} from '@ng-icons/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    importProvidersFrom(NgIconsModule),
+    provideIcons({ lucideBox, lucideTriangleAlert }),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
-import {LobbyComponent} from '../lobby/lobby.component';
-import {LobbyButtonComponent} from '../../shared/components/lobby-button/lobby-button.component';
+import { NgIconsModule, provideIcons } from '@ng-icons/core'; // ✅ IMPORT THIS
+import { lucideBox, lucideTriangleAlert } from '@ng-icons/lucide';
+
+import {
+  HlmAlertDirective,
+  HlmAlertDescriptionDirective,
+  HlmAlertIconDirective,
+  HlmAlertTitleDirective,
+} from '@spartan-ng/ui-alert-helm';
+import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
+
+import { LobbyComponent } from '../lobby/lobby.component';
+import { LobbyButtonComponent } from '../../shared/components/lobby-button/lobby-button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [LobbyComponent, LobbyButtonComponent],
-  templateUrl: './home.component.html',
   standalone: true,
+  templateUrl: './home.component.html',
+  imports: [
+    LobbyComponent,
+    LobbyButtonComponent,
+    NgIconsModule, // ✅ <-- REQUIRED
+    HlmAlertDirective,
+    HlmAlertDescriptionDirective,
+    HlmAlertIconDirective,
+    HlmAlertTitleDirective,
+    HlmIconDirective,
+  ],
+  providers: [
+    provideIcons({ lucideBox, lucideTriangleAlert }), // ✅ PROVIDE ICONS
+  ],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
