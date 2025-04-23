b4dd54e68592f455585aab411e85300200a1f71b
Mykyta Prykhodko <dlagamekontrol@gmail.com>
2025-04-23 23:04:16 +0200

    Added BehaviorSubject with the switchMap

--- src/app/shared/components/lobby-button/lobby-button.component.ts
+++ src/app/shared/components/lobby-button/lobby-button.component.ts
@@ -1,6 +1,6 @@
 import {Component, inject} from '@angular/core';
 import {LobbyApiService} from '../../../features/lobby/services/lobby-api.service';
-import {BehaviorSubject, catchError, finalize, Observable, of} from 'rxjs';
+import {BehaviorSubject, catchError, finalize, Observable, of, switchMap} from 'rxjs';
 import {LobbyModel} from '../../../core/models/lobby-model';
 import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 import {UserService} from '../../../user.service';
@@ -45,25 +45,31 @@ export class LobbyButtonComponent {
   private lobbiesApiService = inject(LobbyApiService);
   private lobbyState = inject(LobbyStateService);
 
-  lobbies$: Observable<LobbyModel[]> | undefined;
+  private refreshLobbies$ = new BehaviorSubject<void>(undefined);
+  lobbies$: Observable<LobbyModel[]> = this.refreshLobbies$.pipe(
+    switchMap(() => {
+      this.loading$.next(true);
+      this.error$.next(null);
+      return this.lobbiesApiService.getLobbies().pipe(
+        finalize(() => this.loading$.next(false)),
+        catchError((error) => {
+          this.error$.next('Failed to load lobbies.');
+          console.error('Error loading lobbies', error);
+          return of([]);
+        })
+      );
+    })
+  );
   loading$ = new BehaviorSubject<boolean>(false);
-  error$ = new BehaviorSubject<string |   null>(null);
+  error$ = new BehaviorSubject<string | null>(null);
 
-  loadLobbies() {
-    this.loading$.next(true);
-    this.error$.next(null);
-
-    this.lobbies$ = this.lobbiesApiService.getLobbies().pipe(
-      finalize(() => this.loading$.next(false)),
-      catchError((error) => {
-        this.error$.next('Failed to load lobbies.');
-        console.error('Error loading lobbies', error);
-        return of([]);
-      })
-    );
-
+  constructor() {
     this.lobbies$.subscribe((lobbies) => {
       this.lobbyState.setLobbies(lobbies);
     });
+  }
+
+  loadLobbies() {
+    this.refreshLobbies$.next(undefined);
   }
 }
