fb71cca7a0771982b4b3c96a4f0afe5b457b1f6e
Mykyta Prykhodko <dlagamekontrol@gmail.com>
2025-05-23 14:37:31 +0200

    Changed realm identifier

--- src/app/core/config/authCodeFlowConfig.config.ts
+++ src/app/core/config/authCodeFlowConfig.config.ts
@@ -2,7 +2,7 @@ import {environment} from '../../../environments/environment.development';
 import {AuthConfig} from 'angular-oauth2-oidc';
 
 export const authCodeFlowConfig: AuthConfig = {
-  issuer: environment.keyCloakUrl + '/realms/FSA',
+  issuer: environment.keyCloakUrl + '/realms/WinDat',
   redirectUri: environment.appUrl + '/',
   clientId: 'fsa-client',
   scope: 'openid',
